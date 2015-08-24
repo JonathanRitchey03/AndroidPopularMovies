c2588ec8e7b6c0e26ec6cc50107914ca8a106eb5
Jonathan Ritchey <jonathan@kurbo.com>
2015-08-23 21:04:18 -0700

    Add FetchWeather to test network fill model

--- Movies/app/src/main/java/com/jonathanritchey/movies/model/MovieModel.java
+++ Movies/app/src/main/java/com/jonathanritchey/movies/model/MovieModel.java
@@ -5,39 +5,24 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Helper class for providing sample content for user interfaces created by
- * Android template wizards.
- * <p/>
- * TODO: Replace all uses of this class before publishing your app.
- */
 public class MovieModel {
 
-    /**
-     * An array of sample (dummy) items.
-     */
-    public static List<MovieItem> ITEMS = new ArrayList<MovieItem>();
+    private static boolean VERBOSE_LOGGING = true;
+    private static String LOG_TAG = MovieModel.class.getSimpleName();
 
-    /**
-     * A map of sample (dummy) items, by ID.
-     */
+    public static List<MovieItem> ITEMS = new ArrayList<MovieItem>();
     public static Map<String, MovieItem> ITEM_MAP = new HashMap<String, MovieItem>();
 
-    static {
-        // Add 3 sample items.
-        addItem(new MovieItem("1", "Item 1"));
-        addItem(new MovieItem("2", "Item 2"));
-        addItem(new MovieItem("3", "Item 3"));
-    }
-
-    private static void addItem(MovieItem item) {
+    public static void addItem(MovieItem item) {
         ITEMS.add(item);
         ITEM_MAP.put(item.id, item);
     }
 
-    /**
-     * A dummy item representing a piece of content.
-     */
+    public static void clear() {
+//        ITEMS = new ArrayList<MovieItem>();
+//        ITEM_MAP = new HashMap<String,MovieItem>();
+    }
+
     public static class MovieItem {
         public String id;
         public String content;
